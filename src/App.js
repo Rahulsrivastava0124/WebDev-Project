f20e2715f2f0af08b3a3e5836a89e3e293fe66fd
sample_demo01 <kumasourav890@gmail.com>
2024-02-11 02:39:38 +0530

    addition of navbar and footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import HomePage from './Components/HomePage';
 import CardContainer from './Components/Cards/CardContainer';
+import Footer from './Components/Footer/Footer';
+import Navbar from './Components/NavBar/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <HomePage />
       <CardContainer />
+      <Footer />
       
     </div>
   );
