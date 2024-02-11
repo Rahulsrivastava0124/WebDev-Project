885e949f58f2ddcc8d8ee4c957958098748ed7c4
Rahul srivastava <87309230+Rahulsrivastava0124@users.noreply.github.com>
2024-02-11 11:40:15 +0530

    Update navigation bar and list of items.

--- src/Components/listApi.js
+++ src/Components/listApi.js
@@ -2,122 +2,37 @@ const ListItem = [
     {
         id: 1,
         title: "HTML",
-        color: "lightblue"
+
     },
     {
         id: 2,
         title: "JAVA",
-        color: "blanchedalmond"
-      },
-    {
-        id: 3,
-        title: "PYTHON",
-        color: "lightgreen"
-       },
-    {
-        id: 4,
-        title: "C",
-        color:"lightgrey"
-       },
-    {
-        id: 5,
-        title: "C++",
-        color: "lightgreen"
-      },
-    {
-        id: 6,
-        title: "C#",
-        color: "lightgrey"
-      },
-    {
-        id: 7,
-        title: "GO LANG",
-        color: "lightyellow"
-     },
-    {
-        id: 8,
-        title: "JAVASCRIPT",
-        color: "lightblue"
-     },
-    {
-        id: 9,
-        title: "COBOL",
-        color: "blanchedalmond"
-     },
-    {
-        id: 10,
-        title: "SWIFT",
-        color:"aquamarine"
+
     },
     {
         id: 11,
         title: "REACT",
-        color: "azure"
-      },
-    {
-        id: 12,
-        title: "KOTLIN",
-        color:"lightgrey"
-     },
-    {
-        id: 13,
-        title: "DART",
-        color: "lightgreen"
-     },
-    {
-        id: 14,
-        title: "CSS",
-        color: "lightgrey"
-     },
-    {
-        id: 15,
-        title: "NODE JS",
-        color: "lightblue"
+
     },
     {
         id: 16,
         title: "EXPRESS JS",
-        color: "aquamarine"
-      },
-    {
-        id: 17,
-        title: "DATABASE",
-        color: "blanchedalmond"
-     },
-    {
-        id: 18,
-        title: "MONGO DB",
-        color: "lightyellow"
+
     },
     {
         id: 19,
         title: "SQL",
-        color:"azure"
+
     },
     {
         id: 20,
         title: "LINUX",
-        color: "lightgreen"
+
     },
     {
         id: 21,
         title: "POSTGRESQL",
-        color: "lightblue"
-    },
-    {
-        id: 22,
-        title: "ANGULAR",
-        color:"aquamarine"
-     },
-    {
-        id: 23,
-        title: "PHP",
-        color: "lightgrey"
-     },
-    {
-        id: 24,
-        title: "XML",
-        color: "blanchedalmond"
+
     },
 ];
 
